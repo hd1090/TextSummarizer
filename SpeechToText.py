812fcede4f63a97d434a891a0a42fe7c2d4a78cb
Harshvardhan Dudeja <harshvardhandudeja@HarshvadhansAir.localdomain>
2019-02-24 05:06:52 -0500

    Google Cloud Speech - Audio Summarization

--- SpeechToText.py
+++ SpeechToText.py
@@ -1,7 +1,12 @@
 import speech_recognition as sr
+import io
+from google.cloud import speech_v1p1beta1 as speech
+import os
 
 def translate(audio_file):
+    return translateGoogle(audio_file)
 
+def translate_SR(audio_file):
     r = sr.Recognizer()
 
     file = sr.AudioFile(audio_file)
@@ -10,3 +15,23 @@ def translate(audio_file):
 
     text = r.recognize_google(audio)
     return text
+
+def translateGoogle(audio_file):
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="Creds.json"
+    client = speech.SpeechClient()
+    content = audio_file.read()
+    audio = speech.types.RecognitionAudio(content=content)
+    config = speech.types.RecognitionConfig(
+        encoding=speech.enums.RecognitionConfig.AudioEncoding.LINEAR16,
+        sample_rate_hertz=8000,
+        language_code='en-US',
+        # Enable automatic punctuation
+        enable_automatic_punctuation=True)
+
+    response = client.recognize(config, audio)
+    for i, result in enumerate(response.results):
+        alternative = result.alternatives[0]
+        print('-' * 20)
+        print('First alternative of result {}'.format(i))
+        print('Transcript: {}'.format(alternative.transcript))
+    return response.results
--- TextSummarizer.py
+++ TextSummarizer.py
@@ -22,7 +22,7 @@ def summarize(audio_file):
       #sentences.append(sent_tokenize(s))
     #sentences = [y for x in sentences for y in x] # flatten list
     text = str(translate(audio_file))
-    sentences = text.split()
+    sentences = re.findall(r'(?:\d[,.]|[^,.])*(?:[,.]|$)', text)
     return summarize_sentences(sentences)
 
 
@@ -41,12 +41,16 @@ def summarize_sentences(sentences):
         coefs = np.asarray(values[1:], dtype='float32')
         word_embeddings[word] = coefs
     f.close()
+
+
     # remove punctuations, numbers and special characters
     clean_sentences = pd.Series(sentences).str.replace("[^a-zA-Z]", " ")
     # make alphabets lowercase
     clean_sentences = [s.lower() for s in clean_sentences]
     stop_words = stopwords.words('english')
     clean_sentences = [remove_stopwords(r.split(), stop_words) for r in clean_sentences]
+
+
     sentence_vectors = []
     for i in clean_sentences:
         if len(i) != 0:
@@ -54,6 +58,8 @@ def summarize_sentences(sentences):
         else:
             v = np.zeros((100,))
         sentence_vectors.append(v)
+
+
     # similarity matrix
     sim_mat = np.zeros([len(sentences), len(sentences)])
     for i in range(len(sentences)):
@@ -61,15 +67,23 @@ def summarize_sentences(sentences):
             if i != j:
                 sim_mat[i][j] = \
                 cosine_similarity(sentence_vectors[i].reshape(1, 100), sentence_vectors[j].reshape(1, 100))[0, 0]
+
+
     nx_graph = nx.from_numpy_array(sim_mat)
     scores = nx.pagerank(nx_graph)
     ranked_sentences = sorted(((scores[i], s) for i, s in enumerate(sentences)), reverse=True)
+
+
     summarized_text = []
     # Extract top 10 sentences as the summary
     topwords = []
+
     for i in range(int(len(sentences) / 3)):
         topwords.append(ranked_sentences[i][1])
+
     for j in range(len(sentences)):
         if sentences[j] in topwords:
             summarized_text.append(sentences[j])
-    return "[{ 'results' : " + str(summarized_text) + "}]"
+
+
+    return ' '.join(summarized_text)
